cda52fbdb4630a2b82fded8a453caa0318d55d9b
kudlaynj <167016811+kudlaynj@users.noreply.github.com>
2024-10-10 18:56:55 +0300

    Update views.py

--- task2/views.py
+++ task2/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
 # Create your views here.
 def index(request):
     return render(request, 'func_template.html')
+
+
+class Index2(TemplateView):
+    template_name = 'class_template.html'
 
